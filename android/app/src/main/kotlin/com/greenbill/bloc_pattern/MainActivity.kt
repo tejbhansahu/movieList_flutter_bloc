903950d44856d5068fc4186adcf82755158f7613
tejbh <tejbhansahu0.ts@gmail.com>
2022-06-25 11:44:15 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/greenbill/bloc_pattern/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.greenbill.bloc_pattern
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/resources/movie_api_provider.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' show Client, Response;
+import '../models/item_model.dart';
+import '../models/trailer_model.dart';
+
+class MovieApiProvider {
+  Client client = Client();
+  final _apiKey = 'a80f58249b9fcbb65b09dc3747eee5fc';
+  final _baseUrl = "https://api.themoviedb.org/3/movie";
+
+  Future<ItemModel> fetchMovieList() async {
+    Response response;
+    if(_apiKey != 'api-key') {
+       response = await client.get(Uri.parse("$_baseUrl/popular?api_key=$_apiKey"));
+    }else{
+      throw Exception('Please add your API key');
+    }
+    if (response.statusCode == 200) {
+      // If the call to the server was successful, parse the JSON
+      return ItemModel.fromJson(json.decode(response.body));
+    } else {
+      // If that call was not successful, throw an error.
+      throw Exception('Failed to load post');
+    }
+  }
+
+  Future<TrailerModel> fetchTrailer(int movieId) async {
+    final response =
+        await client.get(Uri.parse("$_baseUrl/$movieId/videos?api_key=$_apiKey"));
+
+    if (response.statusCode == 200) {
+      return TrailerModel.fromJson(json.decode(response.body));
+    } else {
+      throw Exception('Failed to load trailers');
+    }
+  }
+}
